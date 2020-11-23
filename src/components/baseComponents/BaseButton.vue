bcc4d7b27a2a2ae8b7e4724acdd5739bf8cfb7d9
Witold Malkowski <58203225+Witek120782@users.noreply.github.com>
2020-11-23 20:13:08 +0100

    BaseButton component created

---
+++ src/components/baseComponents/BaseButton.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="baseButton">
+		<slot></slot>
+		<p v-if="modeType">check v-if</p>
+	</div>	
+</template>
+
+<script>
+import { toRefs, computed } from 'vue';
+export default {
+props: ['mode'],
+setup(props){
+	const { mode } = toRefs(props);
+
+	const modeType = computed(()=>{
+		console.log(mode);
+		if (mode.value === 'button'){
+			return true
+		} else {return false}
+	});
+
+	return{
+		modeType
+	}
+}
+
+}
+</script>
+
+<style scoped>
+.baseButton{
+	box-sizing: border-box;
+	padding: 2px;
+	border: 2px solid black;
+	text-align: center;
+	box-shadow: 3px 3px 2px lightgray;
+}
+
+.baseButton:hover{
+	background-color: lightgrey;
+}
+</style>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,3 +1,3 @@
 <template>
-	<h1>mane page</h1>
+	<h1>main page</h1>
 </template>
