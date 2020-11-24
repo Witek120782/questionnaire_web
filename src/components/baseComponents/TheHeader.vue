b549ce0b0b7ecb188d544af57d6aed7cd92739ac
Witold Malkowski <58203225+Witek120782@users.noreply.github.com>
2020-11-24 05:51:55 +0100

    TheHeader component created

---
+++ src/components/baseComponents/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="wrapper">
+	<h1>title of the page</h1>
+	<base-button toLink="/" mode="link">Home Page</base-button>
+	<base-button toLink="/questionnaire" mode="link">Questionnaires</base-button>
+	<base-button toLink="/login" mode="link">login</base-button>
+</div>
+</template>
+
+<style scoped>
+	.wrapper{
+		width: 100;
+		height: 10vh;
+		margin:0;
+		padding: 20px 10% 20px 10%;
+		background-image: url('../../img/backgrounds/background1.gif');
+		background-repeat: no-repeat;
+		background-size:cover;
+		color: white;
+		text-align: center;
+	}
+</style>
