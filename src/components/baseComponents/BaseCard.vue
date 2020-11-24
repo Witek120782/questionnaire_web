36fc252ba2029172aeee1e679a457b54bbc6dbb1
Witold Malkowski <58203225+Witek120782@users.noreply.github.com>
2020-11-24 06:32:13 +0100

    BaseCard component created

---
+++ src/components/baseComponents/BaseCard.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="baseCardWrapper">
+		<div class="title">tytuł</div>
+		<div class="photo">photo</div>
+		<div class="rating">rating</div>
+	</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style scoped>
+.baseCardWrapper{
+	min-width: 15vw;
+	min-height: 40vh;
+	margin:0;
+	padding: 5px;
+	display: grid;
+	box-sizing: border-box;
+	border: 2px solid black;
+	border-radius: 10px;
+	grid-template-rows: 1fr 7fr 2fr;
+	row-gap: 5px;
+}
+.title{
+	align-self: center;
+	justify-self: center;
+	text-transform: uppercase;
+	background-color: gray;
+}
+.photo{
+	align-self: center;
+	justify-self: center;
+	border-bottom: 1px solid black;
+	border-top: 1px solid black;
+	background-color: honeydew;
+}
+.rating{
+	justify-self: center;
+	background-color: indigo;
+}
+
+</style>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,5 +1,15 @@
 <template>
 	<div>
 	<h1>main page</h1>
+	<base-card></base-card>
 </div>
 </template>
+<script>
+import BaseCard from '../components/baseComponents/BaseCard.vue'
+export default {
+	components:{
+		BaseCard
+	}
+	
+}
+</script>
