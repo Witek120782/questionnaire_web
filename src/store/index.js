e8365fa9755ea070fe8803af8c12352cf4cc4894
Witold Malkowski <58203225+Witek120782@users.noreply.github.com>
2020-12-07 21:13:36 +0100

    add photosModule to store index

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import {createStore} from 'vuex';
 import authenticationsModule from './authentication/index.js';
 import formsModule from './forms/index.js';
+import photosModule from './photos/index.js';
 
 const store = createStore({
 	modules:{
 		auth: authenticationsModule,
-		forms: formsModule
+		forms: formsModule,
+		photos: photosModule
 	}
 
 });
