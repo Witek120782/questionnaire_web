853335d72b9458c1743bb411555c227e36141f6d
Witold Malkowski <58203225+Witek120782@users.noreply.github.com>
2020-12-13 17:30:25 +0100

    add basecard component to QSinglePage

--- src/pages/questionnaires/QSinglePage.vue
+++ src/pages/questionnaires/QSinglePage.vue
@@ -1,14 +1,20 @@
 <template>
 	<div class="single-form">
 		here we are {{form.title}}
+		<base-card title="tytuł kardy" ifRating="true" designRating="true" colourRating="true" :pcsToPolybag="true" :sizes="sizerange"></base-card>
 	</div>
 </template>
 
 <script>
 import { computed } from 'vue'
 import { useStore } from 'vuex'
+import BaseCard from '../components/baseComponents/BaseCard.vue'
+
 export default {
 	props:['id'],
+	components:{
+		BaseCard
+	},
 	setup(props){
 	const store = useStore()
 
