f371abe664e8ac64b2262d1d12280fc2c503ef7a
Witold Malkowski <58203225+Witek120782@users.noreply.github.com>
2020-12-30 07:13:08 +0100

    transform answers structure to can use AnswerDetail component wit v-for

--- src/components/baseComponents/BaseAnswer.vue
+++ src/components/baseComponents/BaseAnswer.vue
@@ -3,7 +3,9 @@
 		<div class="optionName">{{optionName}}</div>
 		<div class="optionPhoto"><img :src="optionPhoto" alt=""/></div>
 		<div class="optionData">
-			<answer-detail :v-if="colourValue.lenght>0" title="colourValue" :answerData="colourValue"/>
+			<div v-for="answer in answersDetails" :key="answer.name">
+				<answer-detail :title="answer.name" :answerData="answer.data"/>
+			</div>
 		</div>	
 	</div>	
 </template>
@@ -22,24 +24,31 @@ export default {
 	},
 	setup(props){
 
-		const colourValue = ref([])
-		console.log(props.optionData)
-		if (props.optionData.ratingColourValue.length > 0 ){
-			colourValue.value.push({
-				label: '-',
-				value: props.optionData.ratingColourValue.filter(item=>{if(item=='bad') return item}).length
-			})
-			colourValue.value.push({
-				label: '0',
-				value: props.optionData.ratingColourValue.filter(item=>{if(item=='neutral') return item}).length
-			})
-			colourValue.value.push({
-				label: '+',
-				value: props.optionData.ratingColourValue.filter(item=>{if(item=='good') return item}).length
-			})
+		const answersDetails = ref([])
+		for(let item in props.optionData){
+			if (props.optionData[item].length > 0 ){
+				if (item.includes("Value")){
+					answersDetails.value.push(
+					{name: item,
+						data: [{
+							label: '-',
+							value: props.optionData[item].filter(item=>{if(item=='bad') return item}).length
+						},
+						{
+							label: '0',
+							value: props.optionData[item].filter(item=>{if(item=='neutral') return item}).length
+						},
+						{
+							label: '+',
+							value: props.optionData[item].filter(item=>{if(item=='good') return item}).length
+						}]
+					})
+				}
+			}
 		}
+
 		return{
-			colourValue
+			answersDetails,
 		}
 	}
 }
