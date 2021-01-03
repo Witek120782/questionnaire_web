5c78bb399c402bbbf301eb896e2526e749988165
Witold Malkowski <58203225+Witek120782@users.noreply.github.com>
2021-01-03 18:44:30 +0100

    TheFooter component created and add to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,34 @@
 <template>
-<div class="wrapper">
-	<the-header></the-header>
-	<div class="bodyApp">app vue
-	<router-view></router-view>
-	</div>
+<div class="base-wrapper">
+		<the-header></the-header>
+		<div class="bodyApp">app vue
+			<router-view></router-view>
+		</div>
+		<the-footer/>
 	</div>
 </template>
 
 <script>
+import TheFooter from './components/baseComponents/TheFooter.vue'
 import TheHeader from './components/baseComponents/TheHeader.vue'
 import { useStore } from 'vuex'
 export default {
 	components:{
+		TheFooter,
 		TheHeader
 	},
 	setup(){
 		const store = useStore()
 		store.dispatch('auth/tryLogin')
-
 	}
-	
 }
 </script>
 <style>
-*{
-margin:0;
-padding:0;	
-}
-.bodyApp{
-	padding: 20px 5vw 20px 5vw;
-}
-
+	*{
+		margin:0;
+		padding:0;	
+	}
+	.bodyApp{
+		padding: 20px 5vw 20px 5vw;
+	}
 </style>
